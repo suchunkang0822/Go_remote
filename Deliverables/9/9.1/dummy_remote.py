502955c261db496d217f9c6b10aa16e9c8beaf0d
Vishal Giridhar <vishalgiridhar2020@u.northwestern.edu>
2019-12-10 15:06:59 -0600

    new code + 9

---
+++ Deliverables/9/9.1/dummy_remote.py
@@ -0,0 +1,67 @@
+from FrontEnd import *
+from GoRuleChecker import *
+import json
+import random
+import socket
+
+
+class Proxy(GoRuleChecker):
+    Board_size = 9
+
+    def __init__(self):
+        super().__init__()
+        self.player_stone = ""
+        self.HOST = '127.0.0.1'
+        self.PORT = 8888
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def random_coord(self):
+        row = random.randrange(1,self.Board_size)
+        col = random.randrange(1,self.Board_size)
+        return row,col
+
+    @staticmethod
+    def register():
+        # if string == "register":
+        return "remote"
+
+    def receive_stone(self,stone):
+        self.player_stone = stone
+
+    def make_move(self,boards):
+        ref = GoRuleChecker(boards)
+        boards_correct = ref.sixth_resolve_history(self.player_stone)
+        if boards_correct:
+            if random.random() < 0.4:
+                return "pass"
+            else:
+                row, col = self.random_coord()
+                while boards[0][row][col] != " ":
+                    if ref.sixth_resolve_history(self.player_stone, row, col):
+                        return str(col + 1) + "-" + str(row + 1)
+                    else:
+                        row, col = self.random_coord()
+                        continue
+        else:
+            return "This history makes no sense!"
+
+    def receive_and_send(self):
+        self.s.connect((self.HOST, self.PORT))
+        json_obj = json.loads(self.s.recv(6000).decode())
+        print(json_obj[0])
+        if json_obj[0] == "register":
+            name = self.register()
+            
+            self.s.send(json.dumps(self.register()).encode())
+            
+        elif json_obj[0][0] == "receive-stones":
+            self.receive_stone(json_obj[0][1])
+        elif json_obj[0][0] == "make-a-move":
+            self.s.send(self.make_move(json_obj[0][1]).encode())
+
+
+
+
+if __name__ == "__main__":
+    # while True:
+    Proxy().receive_and_send()
